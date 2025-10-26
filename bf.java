62a49fdb977f12a37e0b3e82f312999c5d116625
Bruce Li <isanoob1897@outlook.com>
2025-10-26 10:23:39 -0700

    start work

--- bf.java
+++ bf.java
@@ -4,4 +4,21 @@ public class bf {
     public static void main(String args[]) {
 
     }
+    public static void interpret(char[] tokens) throws ArrayIndexOutOfBoundsException {
+        byte[] cells = new byte[30000];
+        // index of the pointer
+        int ptr = 0;
+        for (char i:tokens) {
+            if (i=='>') {
+                ptr += 1;
+            } else if (i=='<'&&ptr>0) {
+                ptr -=1;
+            } else if (i=='+') {
+                cells[ptr]+=1;
+            } else if (i=='-') {
+                cells[ptr]-=1;
+            } 
+            // todo
+        }
+    }
 }
